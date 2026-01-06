a641dff745a4321025053a3c46856107edc4a63a
prasanna-0911 <prasannakelaskar112004@gmail.com>
2026-01-06 20:25:11 +0530

    Update __init__.py
    → controls what gets imported with from trackers import *

--- phase3_tracking/trackers/__init__.py
+++ phase3_tracking/trackers/__init__.py
@@ -1,4 +1,9 @@
+"""
+Trackers module for DeepSORT tracking.
+"""
 
 from .kalman_filter import KalmanFilter
 from .track import Track, TrackState
 from .deepsort import DeepSORTTracker
+
+__all__ = ['KalmanFilter', 'Track', 'TrackState', 'DeepSORTTracker']
